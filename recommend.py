427a20509bd3fad0bb1b5a52cab5f04365e44d80
ravinacgardare-design <ravinacgardare@gmail.com>
2026-07-04 02:26:28 +0530

    doing function

---
+++ recommend.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title(" Recommendation Product ")
+st.subheader("Bresed with streamlit")
+st.text("Welcome to your first Recommendation Product")
+st.write("choose your fav. Product")
---
+++ streamlit-practice/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from streamlit-practice!")
+
+
+if __name__ == "__main__":
+    main()
