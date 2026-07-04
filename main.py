f0e1bce8aa9487443cf7506f85c56423e4b81393
ravinacgardare-design <ravinacgardare@gmail.com>
2026-07-04 17:43:35 +0530

    new change

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def main():
-    print("Hello from project!")
+import streamlit as st
 
 
-if __name__ == "__main__":
-    main()
+st.title("streamlit Tutorial")
--- recommend.py
+++ recommend.py
@@ -4,3 +4,25 @@ st.title(" Recommendation Product ")
 st.subheader("Bresed with streamlit")
 st.text("Welcome to your first Recommendation Product")
 st.write("choose your fav. Product")
+
+product=st.selectbox("select your product :",["select","hp","dell","lenovo","boat","phone"])
+
+st.write(f"your choose {product}. Excellent choise")
+
+st.success("best")
+
+if st.button("Press"):
+    st.success("best of luck")
+
+
+add_masala=st.checkbox("add suggestion")
+
+if add_masala:
+    st.write("masala added to your chai")
+
+tea_type=st.radio("pick your chai base:",["milk", "water", "almond Mill"])
+st.write(f"selected base {tea_type}")
+
+flavour=st.selectbox("choose flavour:",["adrak","kesar","Tulsi"])
+st.write(f"selected flavour {flavour}")
+
